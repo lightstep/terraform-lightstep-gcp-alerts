dddfbdcfae9f6d161606abb83cff911b428f24f2
adil.sultan <adil.sultan@lightstep.com>
2022-03-10 12:03:46 -0800

    Creating the repo for GCP alerts via tf
    creating dir: modules, examples and associated tf files and yml files

---
+++ modules/router-alerts/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = ">= 1.60.0"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+resource "lightstep_metric_condition" "compute_cpu_usage" {
+  project_name = var.lightstep_project
+  name         = "Compute CPU Utilization"
+
+  expression {
+    evaluation_window   = "2m"
+    evaluation_criteria = "on_average"
+    is_multi            = true
+    operand             = "above"
+    thresholds {
+      warning  = 80
+      critical = 90
+    }
+  }
+
+  metric_query {
+    metric              = "router.googleapis.com/nat/nat_allocation_failed"
+    query_name          = "a"
+    timeseries_operator = "last"
+    hidden              = false
+    display             = "line"
+
+
+    group_by {
+      aggregation_method = "max"
+      keys               = ["instance_id"]
+    }
+  }
+
+}
